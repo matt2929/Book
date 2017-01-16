ecea8a4dac739c4e0e96710168cac4a1ee697927
matt2929 <matthewstafford29@gmail.com>
2017-01-16 18:03:31 -0500

    Roation Page One Added

--- app/src/main/java/com/example/matthew/book/fragments/PageOne.java
+++ app/src/main/java/com/example/matthew/book/fragments/PageOne.java
@@ -6,20 +6,24 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.matthew.book.Activities.PageTurner;
 import com.example.matthew.book.R;
+import com.example.matthew.book.Util.RotationalMovement;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,17 +38,23 @@ public class PageOne extends Page implements View.OnTouchListener {
     String thisPagesText = "So, my life is a cycle: from seed, to sapling, to full-grown tree, to blossom, to apple, and back to seed again.";
     Button seed, seedling, tree, treeFlower, treeApple;
     Button[] buttons;
-
-    HashMap<Button,Boolean> didITouch= new HashMap<Button,Boolean>();
+RotationalMovement rotationalMovement;
+    HashMap<Button, Boolean> didITouch = new HashMap<Button, Boolean>();
+    int count =0;
     Drawable drawable;
-    String strings[]= {"Seed","Sapling","Young Tree","Tree With Flowers", "Tree With Apples"};
-    Boolean currentBool=false;
+    String strings[] = {"Seed", "Sapling", "Young Tree", "Tree With Flowers", "Tree With Apples"};
+    Boolean currentBool = false;
+    public Handler handler;
+    RelativeLayout relativeLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View viewHierarchy =
                 inflater.inflate(R.layout.fragmentpage1, container, false);
+        handler = new Handler();
+
         seed = (Button) viewHierarchy.findViewById(R.id.cycle1);
         seed.setBackgroundResource(R.drawable.seedling);
         seedling = (Button) viewHierarchy.findViewById(R.id.cycle2);
@@ -55,15 +65,37 @@ public class PageOne extends Page implements View.OnTouchListener {
         treeFlower.setBackgroundResource(R.drawable.treewithflowers);
         treeApple = (Button) viewHierarchy.findViewById(R.id.cycle5);
         treeApple.setBackgroundResource(R.drawable.treewithapples);
-        buttons = new Button[]{seed,seedling, tree, treeFlower, treeApple};
-        PageTurner.allButtons =  new ArrayList<>(Arrays.asList(buttons));
-        for(int i=0;i<buttons.length;i++){
-            didITouch.put(buttons[i],false);
+        relativeLayout = (RelativeLayout) viewHierarchy.findViewById(R.id.nospinzone);
+        buttons = new Button[]{seed, seedling, tree, treeFlower, treeApple};
+        PageTurner.allButtons = new ArrayList<>(Arrays.asList(buttons));
+
+        for (int i = 0; i < buttons.length; i++) {
+            didITouch.put(buttons[i], false);
             buttons[i].setOnTouchListener(this);
             buttons[i].setTextColor(Color.WHITE);
-            buttons[i].setShadowLayer(10,10,10,Color.BLACK);
+            buttons[i].setShadowLayer(10, 10, 10, Color.BLACK);
         }
+        viewHierarchy.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (count <= 1) {
+                    float degreevariation = 360f / 5f;
+                    float degreeCount = 0f;
+                    for (int i = 0; i < buttons.length; i++) {
+                        buttons[i].setX((relativeLayout.getWidth() / 2) + (float) (Math.cos(Math.toRadians(degreeCount)) * 250f) - (buttons[i].getWidth() / 2));
+                        buttons[i].setY((relativeLayout.getHeight() / 2) + (float) (Math.sin(Math.toRadians(degreeCount)) * 250f) - (buttons[i].getHeight() / 2));
+                        degreeCount += degreevariation;
+                    }
+                    rotationalMovement = new RotationalMovement(relativeLayout.getWidth() / 2, relativeLayout.getHeight() / 2, 250f, buttons);
+                    final ClockOne clock = new ClockOne(handler, relativeLayout.getWidth() / 2, relativeLayout.getHeight() / 2);
+                    clock.run();
+                }
+                count++;
+            }
+
+        });
         enabledisabletouch(false);
+
         return viewHierarchy;
 
 
@@ -73,24 +105,24 @@ public class PageOne extends Page implements View.OnTouchListener {
     public boolean onTouch(View view, MotionEvent event) {
 
         Button imageView = (Button) view;
-        didITouch.put(imageView,true);
+        didITouch.put(imageView, true);
         if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
-            buttons = new Button[]{seed,seedling, tree, treeFlower, treeApple};
-            PageTurner.allButtons =  new ArrayList<>(Arrays.asList(buttons));
+            buttons = new Button[]{seed, seedling, tree, treeFlower, treeApple};
+            PageTurner.allButtons = new ArrayList<>(Arrays.asList(buttons));
             drawable = imageView.getBackground();
             view.setBackgroundResource(R.drawable.emptybox);
-           if(imageView.equals(seed)){
+            if (imageView.equals(seed)) {
                 imageView.setText(strings[0]);
-            }else if(imageView.equals(seedling)){
+            } else if (imageView.equals(seedling)) {
                 imageView.setText(strings[1]);
 
-            }else if(imageView.equals(tree)){
+            } else if (imageView.equals(tree)) {
                 imageView.setText(strings[2]);
 
-            }else if(imageView.equals(treeFlower)){
+            } else if (imageView.equals(treeFlower)) {
                 imageView.setText(strings[3]);
 
-            }else if(imageView.equals(treeApple)){
+            } else if (imageView.equals(treeApple)) {
                 imageView.setText(strings[4]);
 
             }
@@ -120,8 +152,8 @@ public class PageOne extends Page implements View.OnTouchListener {
 
     @Override
     public boolean doneTouching() {
-        for(Boolean b: didITouch.values()){
-            if(b==false){
+        for (Boolean b : didITouch.values()) {
+            if (b == false) {
                 return false;
             }
         }
@@ -130,8 +162,25 @@ public class PageOne extends Page implements View.OnTouchListener {
 
     @Override
     public void enabledisabletouch(boolean b) {
-        currentBool=b;
+        currentBool = b;
 
+
+    }
+
+    class ClockOne implements Runnable {
+        private Handler handler;
+
+        public ClockOne(Handler handler, float x, float y) {
+            this.handler = handler;
+
+        }
+
+        public void run() {
+            rotationalMovement.rotate();
+            rotationalMovement.nextPos();
+            handler.postDelayed(this, 100);
+
+        }
 
     }
 }
